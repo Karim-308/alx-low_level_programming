d7f91c5993652c66a5ab644501e375c4a3dfd319
Karim-308 <2001118@eng.asu.edu.eg>
2023-07-13 20:15:02 -0700

    done all c files

---
+++ 0x00-hello_world/5-prinf.c
@@ -0,0 +1,13 @@
+#include <std.io>
+/**
+ * main -Entry point 
+ * Return : Always 0 (sucess)
+ * 
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <std.io>
+/**
+ * main -Entry point 
+ * Return : Always 0 (sucess)
+ * 
+ */
+
+int main (void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}

52d25b3f62fb1e278c24237f793e922778d858ea
Karim-308 <2001118@eng.asu.edu.eg>
2023-07-15 02:09:23 -0700

    Again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- * main -Entry point 
- * Return : Always 0 (sucess)
- * 
+ * main - main block
+ * Return: 0
  */
-
-int main (void)
+int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }

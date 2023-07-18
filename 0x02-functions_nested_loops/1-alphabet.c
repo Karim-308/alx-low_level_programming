c80f1673407011881217e4a8d6af75f6bf0e25aa
Kairm-308 <2001118@eng.asu.edu.eg>
2023-07-18 12:03:55 -0700

    aplphapet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,13 @@
 
 void print_alphabet(void)
 {
-    int i =97;
+	int i =97;
     
-    for (; i<=122; i++)
-    {
-        _putchar(i+'0');
-    }
-    _putchar('\n');
+	for (; i<=122; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
 
 }

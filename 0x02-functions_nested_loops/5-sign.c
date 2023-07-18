b85424d6c640d7fc4e34aacebbd1158925c9d01d
Kairm-308 <2001118@eng.asu.edu.eg>
2023-07-18 13:30:51 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - return 0 letter not lowercase, 1 letter if is lowercase
+ *
+ * @n: int to be checked
+ *  Return: Always 0 (success)
+  */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+
+	else if (n == 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _abs - return 0 the positive of any number 
+ *
+ * @n: int to be checked
+ *  Return: Always 0 (success)
+  */
+int _abs(int)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	else if (n > 0)
+	{
+		return (n);
+	}
+
+	else
+	{
+		return (-1 * n );
+	}
+
+}

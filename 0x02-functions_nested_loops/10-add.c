cc1c0f1044695758428d85eb6b4c972724805910
Kairm-308 <2001118@eng.asu.edu.eg>
2023-07-18 14:58:37 -0700

    Hello World

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add -prints  the addition of two numbers
+ * @y: print int y
+ * @x: print int x
+ * Return: Always 0.
+ */
+
+int add(int y, int x)
+{
+	return (y + x);
+}

3b2a09bea0f4145e31247d33a324956342708c7d
Kairm-308 <2001118@eng.asu.edu.eg>
2023-07-18 13:13:07 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _islower- check if lower case or not
+ * _isalpha- check if the value is a letter or not
  * @c: contains value to be compared
  * Return: 1 (success)
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c > 'a' && c < 'z' || c > 'A' && c < 'Z')
 	{

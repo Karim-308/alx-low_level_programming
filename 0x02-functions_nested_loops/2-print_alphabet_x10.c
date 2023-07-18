595f92de7b439fd349e63de60b4c000eb7577a0b
Kairm-308 <2001118@eng.asu.edu.eg>
2023-07-18 12:39:46 -0700

    aplphapet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,14 @@
  * Return: 0 (success)
  */
 
-void print_alphabet_x10 (void)
+void print_alphabet_x10(void)
 {
-	int i = 97;
+	int i;
 	int j = 1;
 
 	for (; j <= 10; j++)
 	{
+	i = 97;
 	for (; i <= 122; i++)
 		{
 			_putchar(i);

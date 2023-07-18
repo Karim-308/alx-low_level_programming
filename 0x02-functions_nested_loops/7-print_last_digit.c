dd03f39152bf103a0f24828931c01a4ca08231e2
Kairm-308 <2001118@eng.asu.edu.eg>
2023-07-18 14:17:43 -0700

    Hello World

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * print_last_digit - return 0 the positive of any number 
+ * print_last_digit - return 0 the positive of any number
  *
  * @n: int to be checked
  *  Return: Always 0 (success)
   */
 int print_last_digit(int n)
 {
-	 if (n >=  0)
-	{	
-		n%=10;
-		_putchar(n+'0');
-		return n;
+	if (n >=  0)
+	{
+		n %= 10;
+		_putchar(n + '0');
+		return (n);
 	}
 
 	else
 	{
-		n%=10;
-		n*=-1;
-		_putchar(n+'0');
+		n %= 10;
+		n *= -1;
+		_putchar(n + '0');
 		return (n % 10);
 	}
 

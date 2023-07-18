3ff5450f7dc833104237aa849a4ab550bc4a1ae6
Kairm-308 <2001118@eng.asu.edu.eg>
2023-07-18 11:54:24 -0700

    aplphapet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alaphpets
+ *
+ * Return: 0 (success)
+ */
+
+void print_alphabet(void)
+{
+    int i =97;
+    
+    for (; i<=122; i++)
+    {
+        _printchar(i+'0');
+    }
+    _printchar('\n'};
+
+}

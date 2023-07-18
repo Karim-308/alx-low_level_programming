c64feca0019c601cb94181317f01c542e901ac55
Kairm-308 <2001118@eng.asu.edu.eg>
2023-07-18 15:09:59 -0700

    Lost

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * times_table - prints time tables
+ *
+ * Return: Always 0  (succes)
+ */
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+
+			if ((z / 10) == 0)
+			{
+				if (y == 0)
+				{
+					_putchar ('0');
+				}
+				if (y != 0)
+				{
+					_putchar (' ');
+					_putchar ((z % 10) + '0');
+				}
+				if (y < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
+			}
+			else
+			{
+				_putchar ((z / 10) + '0');
+				_putchar ((z % 10) + '0');
+				if (y < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
+			}
+		}
+		_putchar ('\n');
+	}
+}

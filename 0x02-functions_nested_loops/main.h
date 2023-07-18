78f2d1f63dace7a579c8f71dc74c412c3fceec14
Kairm-308 <2001118@eng.asu.edu.eg>
2023-07-18 11:09:07 -0700

    Main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int add(int, int);
+void times_table(void);
+int _putchar(char c);
+int _islower(int c);
+void jack_bauer(void);
+void print_to_98(int n);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void print_alphabet(void);
+void print_alphabet_x10(void);

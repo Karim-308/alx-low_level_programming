6a480e8a03a1d9e3751e0f3e5d3730b81689cf11
Kairm-308 <2001118@eng.asu.edu.eg>
2023-07-18 11:19:56 -0700

    Main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
 #include "main.h"
-/** 
- * main-prints "_putchar"
+
+/**
+ * main - prints "_putchar"
  *
- * returns: 0 (success)
- *
- */ 
+ * Return: 0 (success)
+ */
 
 int main(void)
 {
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return(0);
+    return (0);
 }

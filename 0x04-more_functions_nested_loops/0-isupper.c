f7ec930a02f3622ca6b92db40e96c9aed2c30dcb
Karim-308 <2001118@eng.asu.edu.eg>
2023-07-20 05:30:28 -0700

    0 & 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  _isupper - check if letter is upper case or not
+ * @c : letter to be checked
+ *
+ * Return: 1 if upper 0 if not
+ */
+
+int _isupper (int c)
+{
+	if (c > 'A' && c < 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if the value is a digit or not
+ * @c: contains value to be compared
+ * Return: 1 (success)
+ */
+
+int _isdigit(int c)
+{
+	if ((c > '1' && c < '9'))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}

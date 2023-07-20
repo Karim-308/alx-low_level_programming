b93ffe413c5db897ee8dd8a181188654d8768844
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-07-20 20:07:37 +0300

    Update 9-fizz_buzz.c
    downtownCode

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,10 +4,9 @@
 /**
  * main - entry point
  *
- * Description: prints the numbers 1 - 100 w/
- * Fizz for multiples of 3, Buzz for multiples of 5,
+ * Description: prints the numbers 1 - 100 w
+ * Fizz if multiples of 3, Buzz ig multiples of 5,
  * and FizzBuzz for multiples of both
- *
  * Return: always 0
  */
 int main(void)
@@ -16,12 +15,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0 ) && (i % 5 == 0 ))
+		if (i % 15 == 0)
 			printf("FizzBuzz");
-		else if (i % 5 == 0)
-			printf("Buzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
 			printf("%i", i);
 		if (i < 100)

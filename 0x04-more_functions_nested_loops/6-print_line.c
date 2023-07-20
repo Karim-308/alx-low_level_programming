e8b7f99198e5e3d269f3ce73e079fd5ae73df0a9
Karim-308 <2001118@eng.asu.edu.eg>
2023-07-20 08:59:59 -0700

    updated files number 6 7

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints __ x n 
+ * @n: how many times
+ *
+ * Return: void
+ */
+
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}

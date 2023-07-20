adb2c390397bd1b4e0336d32eed3508488a83deb
Karim-308 <2001118@eng.asu.edu.eg>
2023-07-20 07:48:28 -0700

    updated files

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_numbers - print 0 to 9
+*Return: Doesn't
+*/
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+	_putchar(c+'0');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_most_numbers - print 0 to 9  but not 2,4
+*Return: Doesn't
+*/
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+
+	if ( c == 2 || c == 4 )
+		continue;
+	else
+		_putchar(c+'0');
+
+	}
+	_putchar('\n');
+}

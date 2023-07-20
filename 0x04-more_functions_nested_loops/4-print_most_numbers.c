2ef67f3b1000bd340902fffe6941d2af903ce267
Karim-308 <2001118@eng.asu.edu.eg>
2023-07-20 07:56:59 -0700

    updated files number 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,10 +12,10 @@ void print_most_numbers(void)
 	for (c = 0; c <= 9; c++)
 	{
 
-		if ( c == 2 || c == 4 )
+	if ( c == 2 || c == 4 )
 			continue;
-		else
-			_putchar(c+'0');
+
+	_putchar(c+'0');
 
 	}
 	_putchar('\n');

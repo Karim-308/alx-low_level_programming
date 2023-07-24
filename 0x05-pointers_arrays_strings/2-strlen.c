5f09631cd5959fac04d36572783ec8ea30acfa55
Karim-308 <2001118@eng.asu.edu.eg>
2023-07-24 16:55:53 -0700

    0,1,2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,19 +2,20 @@
 /**
 * _strlen - count str length
 * @s: string to b edetermined
+* return : length of the string
 */
 int _strlen(char *s)
 {
-    int l = 0;
-    int flag = 0;
-	
+	int l = 0;
+	int flag = 0;
+
 	while (flag == 0)
 	{
-		if (*(s+l) != '\0')
+		if (*(s + l) != '\0')
 			l++;
 		else
 			flag = 1;
 	}
-	
-	return l;
+
+	return (l);
 }

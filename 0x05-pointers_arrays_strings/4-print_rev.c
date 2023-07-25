3137e911b7784cdfef4b0d32ef67dda6e23b95b6
Karim-308 <2001118@eng.asu.edu.eg>
2023-07-24 17:31:02 -0700

    3,4,5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -20,3 +20,15 @@ void print_rev(char *s)
 	}
 	_putchar('\n');
 }
+
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}

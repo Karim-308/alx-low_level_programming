2b3505edc5d330ace03e7df36fdbed0ea37af7b2
Karim-308 <2001118@eng.asu.edu.eg>
2023-07-24 18:46:03 -0700

    6,7,8,9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 /**
  * print_array - prints n element of an array of integers
- * Owned by Bwave
  * @a: int to be checked
  * @n: int to be checked
  * Return: 0 is a success
@@ -11,14 +10,11 @@ void print_array(int *a, int n)
 {
 	int x;
 
-	if (n<0)
-		return;
-	
-	for (x = 0; x < n -1 ; x++)
-		
-		printf("%d, ", a[x]);
-		
+	for (x = 0; x < n; x++)
 
-			printf("%d", a[n-1]);
-printf("\n");
+		if (x != n - 1)
+			printf("%d, ", a[x]);
+		else
+			printf("%d", a[x]);
+	printf("\n");
 }

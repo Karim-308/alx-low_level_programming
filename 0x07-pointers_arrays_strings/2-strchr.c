85ba66b31d9347054dcdf28db2bdd92a53e8040d
Karim-308 <2001118@eng.asu.edu.eg>
2023-07-31 16:58:50 -0700

    012

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - looks for a character in a string
+ * @s: string
+ * @c: character
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	int x;
+	for (x = 0; s[x] >= '\0'; x++)
+	{
+		if (s[x] == c)
+		{
+			return (&s[x]);
+		}
+	}
+	return (0);
+}

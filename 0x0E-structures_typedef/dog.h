c421a32517df0a8a519bd1dbc37041768c82ec0b
Karim-308 <2001118@eng.asu.edu.eg>
2023-08-14 16:23:12 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: This struct represents a dog
+ */
+struct dog
+{
+	char* name;
+	int age;
+	char* owner;
+};

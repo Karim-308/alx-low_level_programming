443a8703dcf555b60016dce2c04c760a82c0851c
Karim-308 <2001118@eng.asu.edu.eg>
2023-08-14 18:55:38 -0700

    1-2-3-4

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
+#include <libgen.h>
 
-/**
- * main - prints the name of the file it was compiled from.
- * Return: 0
- */
-int main(void)
-{
-    printf("%s\n", __FILE__);
-    return (0);
+int main() {
+    char* filename = basename(__BASE_FILE__);
+    printf("%s\n", filename);
+    return 0;
 }

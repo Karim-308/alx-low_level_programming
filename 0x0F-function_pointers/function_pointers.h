b1fbcaf794dd959d18ecaea041b2029ef42e9c87
Karim-308 <2001118@eng.asu.edu.eg>
2023-08-17 16:11:02 -0700

    k

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,33 @@
+
+
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
 #include <stddef.h>
 
+
 void print_name(char *name, void (*f)(char *));
-int int_index(int *array, int size, int (*cmp)(int));
+
+
 void array_iterator(int *array, size_t size, void (*action)(int));
-int main(int argc, char *argv[])
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+/* Task 3 */
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+int (*get_op_func(char *s))(int, int);
+
 
 #endif
+

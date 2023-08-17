ecda2e48d88a79af43c6e51b253d89dc24d80591
Karim-308 <2001118@eng.asu.edu.eg>
2023-08-16 19:14:07 -0700

    0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,5 +4,6 @@
 #include <stddef.h>
 
 void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
 
 #endif
--- 0x0F-function_pointers/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stddef.h>
-
-void array_iterator(int *array, size_t size, void (*action)(int));
-
-#endif

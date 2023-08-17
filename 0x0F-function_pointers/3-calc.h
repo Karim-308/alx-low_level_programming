846e7e04ed477a8e1ad4a26e1c3a9a4465b5a665
Karim-308 <2001118@eng.asu.edu.eg>
2023-08-17 16:37:10 -0700

    k

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -4,10 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct op - Struct for mathematical operations
+ * @op: The operator
+ * @f: The function associated
+ *
+ * Description: This struct is used for selecting the correct
+ * mathematical operation function.
+ */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -18,3 +26,4 @@ int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 #endif
+

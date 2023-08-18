ecffddfe750eede0bfcbada6f442e62c4469c432
Karim-308 <2001118@eng.asu.edu.eg>
2023-08-17 18:08:31 -0700

    low ket

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -23,7 +23,7 @@ void print_all(const char * const format, ...)
 			printf("%s%d", sep, va_arg(args, int));
 			break;
 		case 'f':
-			printf("%s%f", sep, va_arg(args, double)); /* float promotes to double in va_arg */
+			printf("%s%f", sep, va_arg(args, double));
 			break;
 		case 's':
 			str = va_arg(args, char *);

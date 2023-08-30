96d3954a6470718410b3c94a8415919736f587bb
Karim-308 <2001118@eng.asu.edu.eg>
2023-08-29 17:39:55 -0700

    betty update for 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,39 +9,36 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *current, *prev_node;
-    unsigned int i;
+	listint_t *current, *prev_node;
+	unsigned int i;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    current = *head;
+	current = *head;
 
-    if (index == 0)
-    {
-        *head = current->next;  /* Change head to point to the next node */
-        free(current);  /* Free the old head */
-        return (1);
-    }
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		return (1);
+	}
 
-    /* Move to the node just before the node to delete */
-    for (i = 0; i < index - 1; i++)
-    {
-        if (current == NULL)  /* If the list is shorter than the index */
-            return (-1);
-        current = current->next;
-    }
+	for (i = 0; i < index - 1; i++)
+	{
+		if (current == NULL)
+			return (-1);
+		current = current->next;
+	}
 
-    if (current == NULL || current->next == NULL)
-        return (-1);
+	if (current == NULL || current->next == NULL)
+		return (-1);
 
-    /* Unlink the node to delete */
-    prev_node = current->next;
-    current->next = prev_node->next;
+	prev_node = current->next;
+	current->next = prev_node->next;
 
-    /* Free the node */
-    free(prev_node);
+	free(prev_node);
 
-    return (1);
+	return (1);
 }
 

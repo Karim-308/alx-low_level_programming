c79004b88074866de87245bf85b775437ba4abf9
Karim-308 <2001118@eng.asu.edu.eg>
2023-08-31 19:54:33 -0700

    2 again with masking

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,12 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 
+	unsigned long int mask = 1UL << index;
+
 	if (index >= (sizeof(unsigned long int) * 8))
 
 		return (-1);
 
-	unsigned long int mask = 1UL << index;
 
 
 	if ((n & mask) != 0)
